7543a0de05b07c75a84b86ee1b12989040272b46
Ntando <mngnta011@myuct.ac.za>
2024-09-20 10:43:05 +0200

    test case 4, multiple of 10 < 100

--- testnumberutil.py
+++ testnumberutil.py
@@ -1,17 +1,21 @@
 """
 >>> from numberutil import aswords
 
-# Test1: test for 0
+ Test1: test for 0
 >>> aswords(0)
 'zero'
 
-# Test2: test for single digit number
+ Test2: test for single digit number
 >>> aswords(5)
 'five'
 
-# Test3: test for a number below 20 and double digit
+ Test3: test for a number below 20 and double digit
 >>> aswords(19)
 'nineteen'
+
+ Test4: test for a multiple of 10 below 100
+>>> aswords(70)
+'seventy'
 """
 
 if __name__ == "__main__":
